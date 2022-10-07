63f394701d6ec4354f337154947ab8ba30d0206f
Mayke9873 <mayke.s.sousa@gmail.com>
2022-10-07 10:54:59 -0300

    Adicionado instancias das classes Nokia e iPhone.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("(11) 9-1111-1111", "Nokia 9", "123456789", 16);
+n1.ApresentarAparelho();
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("WhatsAPP");
+
+Console.WriteLine("\n");
+
+Iphone p1 = new Iphone("(11) 1-9999-9999", "iPhone SE 2018", "987654321", 128);
+p1.ApresentarAparelho();
+p1.Ligar();
+p1.ReceberLigacao();
+p1.InstalarAplicativo("WhatsAPP");
